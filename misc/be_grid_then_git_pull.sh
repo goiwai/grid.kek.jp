83126593c301eb45655169f763c9a4359cb15a7e
Go Iwai <go.iwai@kek.jp>
2016-11-09 11:43:14 +0900

    finally no error occured

--- misc/be_grid_then_git_pull.sh
+++ misc/be_grid_then_git_pull.sh
@@ -1,21 +1,4 @@
 #! /bin/sh
-
-# always fatal...
-# Tagging grid.kek.jp
-# Flushing file system buffers
-# Signing new manifest
-# Remounting newly created repository revision
-# umount: /cvmfs/grid.kek.jp: device is busy.
-#         (In some cases useful info about processes that use
-#          the device is found by lsof(8) or fuser(1))
-# umount: /cvmfs/grid.kek.jp: device is busy.
-#         (In some cases useful info about processes that use
-#          the device is found by lsof(8) or fuser(1))
-# Fatal: encountered a fatal error while transaction...
-# belle.kek.jp (stratum0 / local)  
-# grid.kek.jp (stratum0 / local - in transaction)  
-#
-
 script_name=$(basename $0)
 script_dir=$(cd $(dirname $0) && pwd)
 dir_at_exec=$(cd . && pwd)
@@ -30,15 +13,23 @@ cvmfs_server list | grep -q "^$repo.*stratum0.*local.*$"
 if test $? -eq 0; then
     cd $git_dir && git remote show origin | tail -n 1 | grep -q 'out of date'
     if test $? -eq 0; then # out of date, hence to be updated
+	cd ~
         sudo -i -u $to_user /bin/sh -c "cvmfs_server transaction $repo && cd $git_dir && git pull"
         if test $? -ne 0; then
             echo "Fatal: encountered a fatal error while transaction..." >&2
+	    fuser -va $cvmfs_dir /var/spool$cvmfs_dir/rdonly >&2
+	    lsof | grep $repo >&2
+            sudo -i -u $to_user /bin/sh -c "cvmfs_server abort -f $repo" >&2
     	    cvmfs_server list >&2
     	    exit 1
     	fi
+
         sudo -i -u $to_user /bin/sh -c "cvmfs_server publish $repo"
         if test $? -ne 0; then
             echo "Fatal: encountered a fatal error while publishing..." >&2
+	    fuser -va $cvmfs_dir /var/spool$cvmfs_dir/rdonly >&2
+	    lsof | grep $repo >&2
+            sudo -i -u $to_user /bin/sh -c "cvmfs_server abort -f $repo" >&2
             cvmfs_server list >&2
             exit 1
         fi
