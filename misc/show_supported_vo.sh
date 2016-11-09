38013891e305a4c3c1d6a956115ec2743e317235
Go Iwai <go.iwai@kek.jp>
2016-11-09 15:21:20 +0900

    minor fix because moved into here from ..

--- misc/show_supported_vo.sh
+++ misc/show_supported_vo.sh
@@ -3,14 +3,14 @@ script_name=$(basename $0)
 script_dir=$(cd $(dirname $0) && pwd)
 dir_at_exec=$(cd . && pwd)
 
-vomsdir=$script_dir/etc/grid-security/vomsdir
+vomsdir=$(cd $script_dir/../etc/grid-security/vomsdir && pwd)
 
 for f in $vomsdir/*; do
     if test -d $f; then
-	ls $f/*.lsc > /dev/null 2>&1
-	if test $? -eq 0; then
-	    echo $(basename $f)
-	fi
+        ls $f/*.lsc > /dev/null 2>&1
+        if test $? -eq 0; then
+            echo $(basename $f)
+        fi
     fi
 done
 
